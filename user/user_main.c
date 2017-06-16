42589b80ebc4f7fa3c562b2341230c248e5b17d2
jackkum <jackkum@jackkum.Dlink>
2017-06-16 14:26:37 +0800

    fix indents

--- user/user_main.c
+++ user/user_main.c
@@ -1,4 +1,4 @@
-#define PLATFORM_DEBUG	true
+#define PLATFORM_DEBUG  true
 
 #include "user_config.h"
 #include "ets_sys.h"
@@ -36,9 +36,9 @@ SERVER *bridge, *config;
 
 void delay_ms(uint16 ms)
 {
-	while(ms--){
-		os_delay_us(1000);
-	}
+  while(ms--){
+    os_delay_us(1000);
+  }
 }
 
 /**
@@ -46,22 +46,22 @@ void delay_ms(uint16 ms)
  */
 void ICACHE_FLASH_ATTR user_init()
 {
-	// default initialize uart
-	uart_init(BIT_RATE_9600, BIT_RATE_9600);
+  // default initialize uart
+  uart_init(BIT_RATE_9600, BIT_RATE_9600);
 
-	// init gpio
-	gpio_init();
+  // init gpio
+  gpio_init();
 
-	// config direction of IO
-	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
+  // config direction of IO
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
 
-	// pull up to the ground 
-	gpio_output_set(0, RS484_DIR, RS484_DIR, 0);
+  // pull up to the ground 
+  gpio_output_set(0, RS484_DIR, RS484_DIR, 0);
 
-	// delay
-	delay_ms(100);
+  // delay
+  delay_ms(100);
  
- 	// init wifi
+  // init wifi
   WiFiInit();
 
   // os task for recv bytes from uart
@@ -103,7 +103,7 @@ static void ICACHE_FLASH_ATTR recvTask(os_event_t *events)
  * @param[in]  len   The length
  */
 static void ICACHE_FLASH_ATTR onProxyData(char * data, uint16 len) {
-	// higth level
+  // higth level
   gpio_output_set(RS484_DIR, 0, RS484_DIR, 0);
   // wait
   os_delay_us(1);
@@ -123,58 +123,58 @@ static void ICACHE_FLASH_ATTR onProxyData(char * data, uint16 len) {
  */
 static void ICACHE_FLASH_ATTR onCommandData(char * data, uint16 len) {
 
-	// cast to packet
-	Packet *pack = getPacket(data);
-	// init data from packet
-	Data *d = (Data *) &pack->data;
-	// init answer
-	Packet answer;
-	// init data of answer
-	Data aData;
+  // cast to packet
+  Packet *pack = getPacket(data);
+  // init data from packet
+  Data *d = (Data *) &pack->data;
+  // init answer
+  Packet answer;
+  // init data of answer
+  Data aData;
 
-	// fill headers
-	answer.header.address = DEVICE_ADDR;
-	answer.header.command = pack->header.command;
-	answer.header.length  = 7 + sizeof(Data);
+  // fill headers
+  answer.header.address = DEVICE_ADDR;
+  answer.header.command = pack->header.command;
+  answer.header.length  = 7 + sizeof(Data);
 
-	// request is for me
-	if(checkPacket(pack, DEVICE_ADDR) == PACKET_ERROR_NO_ERROR){
+  // request is for me
+  if(checkPacket(pack, DEVICE_ADDR) == PACKET_ERROR_NO_ERROR){
 
-		// validate crc of packet
-		uint8 error = checkCrc(pack);
+    // validate crc of packet
+    uint8 error = checkCrc(pack);
 
-		// put error to the answer
-		aData.error = error;
+    // put error to the answer
+    aData.error = error;
 
-		// no errors
-		if(error == PACKET_ERROR_NO_ERROR){
-			// check command
-			switch(pack->header.command){
+    // no errors
+    if(error == PACKET_ERROR_NO_ERROR){
+      // check command
+      switch(pack->header.command){
 
-				// command to set params of uart
-				case PACK_COMMAND_SET_DATA:
-					UartDev.baut_rate = (UartBautRate) d->baudrate;
-					UartDev.data_bits = (UartBitsNum4Char) d->bits;
-					UartDev.parity    = (UartParityMode) d->parity;
-					UartDev.stop_bits = (UartStopBitsNum) d->stop;
+        // command to set params of uart
+        case PACK_COMMAND_SET_DATA:
+          UartDev.baut_rate = (UartBautRate) d->baudrate;
+          UartDev.data_bits = (UartBitsNum4Char) d->bits;
+          UartDev.parity    = (UartParityMode) d->parity;
+          UartDev.stop_bits = (UartStopBitsNum) d->stop;
 
-			    uart_config(UART0);
-			    uart_config(UART1);
-				break;
-			}
+          uart_config(UART0);
+          uart_config(UART1);
+        break;
+      }
 
-			// set current params to the answer
-			aData.baudrate = (uint32) UartDev.baut_rate;
-			aData.bits     = (uint8) UartDev.data_bits;
-			aData.parity   = (uint8) UartDev.parity;
-			aData.stop     = (uint8) UartDev.stop_bits;
-		}
+      // set current params to the answer
+      aData.baudrate = (uint32) UartDev.baut_rate;
+      aData.bits     = (uint8) UartDev.data_bits;
+      aData.parity   = (uint8) UartDev.parity;
+      aData.stop     = (uint8) UartDev.stop_bits;
+    }
 
-		// copy params to the answer
-		os_memcpy(&answer.data, (uint8 *)&aData, sizeof(Data));
-		// recalculate crc of packet
-		setCrc(&answer);
-		// answer to the client
-		writeClient(config, (uint8 *)&answer, answer.header.length);
-	}
+    // copy params to the answer
+    os_memcpy(&answer.data, (uint8 *)&aData, sizeof(Data));
+    // recalculate crc of packet
+    setCrc(&answer);
+    // answer to the client
+    writeClient(config, (uint8 *)&answer, answer.header.length);
+  }
 }
